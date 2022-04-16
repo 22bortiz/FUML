5d54e0da2daafd5f4bfe367ef12c0b64d286ddcb
22apr <22apr@DESKTOP-TLOGULS>
2022-04-16 03:09:15 -0500

    first commit

---
+++ fuml/src/application/controller/MainController.java
@@ -0,0 +1,19 @@
+package application.controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    void createNew(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewDiagram(ActionEvent event) {
+
+    }
+
+}
